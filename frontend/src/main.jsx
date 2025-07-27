a055c6ac3791d6f6df7d449d0e54fb4dc79fad00
Farheen Malak <115576023+FarheenMalak@users.noreply.github.com>
2025-07-27 09:39:26 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from './components/App'
 import axios from 'axios';
 
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://feedback-system-liard.vercel.app';
 axios.defaults.withCredentials = true;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
